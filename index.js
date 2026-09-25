be446c1ec9baf35d09d0443ac917fcc67b6cf44f
COMRADE040 <rafiulislam040@gmail.com>
2026-09-25 21:11:25 +0600

    data read

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const data = require('./lib/data')
 //app object - module scaffolding
 const app = {}
 
-data.create('test', 'newFile', {'name': "Bangladesh", 'language': 'Bangla'}, (err) => {
-    console.log(`error was `, err)
+data.read('test', 'newFile', (err, data) => {
+    console.log(err, data)
 })
 
 
